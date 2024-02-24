08c6a1b36c84ccd8cddfdbc0506c659a8b4b21f5
demattei-teo <teitodematteio@gmail.com>
2024-02-24 11:58:28 -0300

    fix: resolve error in deploy

--- src/services/config/firebase.ts
+++ src/services/config/firebase.ts
@@ -1,6 +1,5 @@
 'use client'
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-
-getAnalytics(app)
 
 export const db = getFirestore(app)
